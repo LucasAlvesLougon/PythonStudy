f915b69c1581333f9236f19b5a2fe6a2dd95290d
Lucas Mateus <101680597+LucasAlvesLougon@users.noreply.github.com>
2023-12-11 18:05:20 -0300

    Aulas 1 e 2

---
+++ Aula01.py
@@ -0,0 +1,10 @@
+nome = 'Roger' #String
+idade = 18 #int
+altura = 1.82 #float
+is_male = True / False #bool
+gosto_de = 'Programação'
+
+print(f'Meu nome é {nome}.')
+print(f'Eu gosto de {gosto_de}')
+print(f'Eu tenho {idade} anos')
+print(f'{nome} gosta de {gosto_de} e tem {idade} anos')
---
+++ Aula02.py
@@ -0,0 +1,40 @@
+# https://www.youtube.com/playlist?list=PLj7gJIFoP7jdirAFg-fHe9HKOnGLGXSHZ
+
+num1 = 5
+num2 = 5
+
+a = float(num1)
+b = int(num2)
+
+
+a = int(float('5.3'))
+b = '3.5'
+
+
+# Operadores da Matemática
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 % num2)
+print(num1 ** num2)
+print(num1 // num2)
+
+print(3 + 5 * 7 + 3)
+print('-' * 20)
+print((3 + 5) * (7 + 3))
+
+
+# Funções
+import math
+
+print(abs(-15))
+print(pow(3,3))
+print(max(1,5))
+print(min(1,5))
+print(round(8.8))
+
+
+print(math.floor(8.8)) # Da o Mínimo
+print(math.ceil(8.8)) # Da o Máximo
+print(math.sqrt(10)) # Da a Raiz quadrada
