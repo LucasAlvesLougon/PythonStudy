7376f5ce52ec73848ea8169e62d0cf3c6aa4f80d
Lucas Mateus <101680597+LucasAlvesLougon@users.noreply.github.com>
2023-12-15 14:51:08 -0300

    Create Aula04.py

---
+++ Aula04.py
@@ -0,0 +1,13 @@
+# Aula de strings em python
+
+minha_string = "qualquer,\ntexto 1,\ntexto 2."
+# print(minha_string.isupper())
+# print(minha_string.islower())
+# print(minha_string.strip())
+# print(minha_string[-4:-1])
+# print(minha_string.index("quer"))
+
+# x = "texto" in minha_string
+# print(x)
+
+print(minha_string)
