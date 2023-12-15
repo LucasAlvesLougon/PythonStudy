1662526aed81b4201e1aca4ce248bfef66d2479e
Lucas Mateus <101680597+LucasAlvesLougon@users.noreply.github.com>
2023-12-15 15:54:26 -0300

    Create Aula06.py

---
+++ Aula06.py
@@ -0,0 +1,46 @@
+# Collections, lists e Tuples
+
+familia = ["Roger","Cris","Manu","Vini","Selina"]
+# print(familia[3]) - retorna um indice
+# print(familia[-3]) - retorna um indice de traz para frente
+# print(familia[2:]) - retorna a partir do indice 2
+# print(familia[2:4]) - retorna a partir do indeico até o 4, excluindo o 4
+
+#print(familia)
+#familia[3] = "Roger"
+#print(familia)
+
+#familia.extend(["Fernando","Rosania"])
+#print(familia)
+
+#familia.append("Spock")
+#familia.insert(2,"Spock")
+#familia.pop()
+#familia.pop()
+#familia.remove("Spock")
+
+#print(familia)
+
+#familia.clear()
+
+#print(familia.index("Roger"))
+#print(familia.count("Roger"))
+
+#print(familia)
+
+#idade_familia = [34,36,13,11,2]
+#print(idade_familia)
+#idade_familia.sort() # Ordena do menor pro maior
+#print(idade_familia)
+#idade_familia.sort()
+#idade_familia.reverse()
+#print(idade_familia)
+
+#familia2 = familia.copy()
+#print(familia2)
+#familia.remove("Roger")
+#print(familia2)
+#print(familia)
+
+coordenadas = (-49,-36)
+print(coordenadas.count(-49))
