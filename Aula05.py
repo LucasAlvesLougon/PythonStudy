6ff12fd290407849dd3bba4bf9940c1a2f367e37
Lucas Mateus <101680597+LucasAlvesLougon@users.noreply.github.com>
2023-12-15 15:06:48 -0300

    Create Aula05.py

---
+++ Aula05.py
@@ -0,0 +1,15 @@
+# Input, Comentários e Calculadora Básica
+
+'''
+nome = input("Digite seu nome: ")
+idade = int(input(f"Qual a sua idade {nome}? "))
+
+nascimento = 2023 - idade
+
+print(f"{nome} você nasceu em {nascimento}")
+'''
+
+num1 = int(input("Digite um numero: "))
+num2 = int(input("Digite outro numero: "))
+resultado = num1 + num2
+print(f"O resultado é: {resultado}")
