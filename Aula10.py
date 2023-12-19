cd0e97b8097e8047e39ad1550abd563a428bc4ae
Lucas Mateus <101680597+LucasAlvesLougon@users.noreply.github.com>
2023-12-19 15:19:28 -0300

    Aula10

---
+++ Aula10.py
@@ -0,0 +1,20 @@
+# Coleções: set e dictionary
+
+'''frutas = {"maça", "Laranja","Abacaxi"}
+
+frutas.add("Pera")
+frutas.remove("maça")
+frutas.pop()
+
+print(frutas)'''
+
+'''set1 = {"maça", "Laranja","Abacaxi"}
+set2 = {0,3,50,-74}
+set3 = {True,False,False,False}
+set4 = {"Roger",34,True}
+
+print(set1)
+print(set2)
+print(set3)
+print(set4)'''
+
---
+++ app-dict.py
@@ -0,0 +1,22 @@
+# Dictionary
+
+meses = {
+    "Jan" : "Janeiro",
+    "Fev" : "Fevereiro",
+    "Mar" : "Março",
+    "Abr" : "Abril",
+    "Mai" : "Maio",
+    "Jun" : "Junho",
+    "Jul" : "Julho",
+    "Ago" : "Agosto",
+    "Set" : "Setembro",
+    "Out" : "Outubro",
+    "Nov" : "Novembro",
+    "Dez" : "Dezembro",
+}
+
+'''print(meses["Mai"])
+print(meses.get("Mai"))
+print(meses.get("Abc", "Janeiro"))'''
+
+print(len(meses))
